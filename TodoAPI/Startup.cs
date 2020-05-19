e132c39ba31a8522e22129f3d3c8ec5140a1d53e
rsmith6321 <rsmith@jinnawats-MacBook-Pro.local>
2020-05-19 12:56:48 +0700

    update TodoAPI

--- TodoAPI/Startup.cs
+++ TodoAPI/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
-using TodoApi.Models;
+using TodoAPI.Models;
 
 namespace TodoAPI
 {
